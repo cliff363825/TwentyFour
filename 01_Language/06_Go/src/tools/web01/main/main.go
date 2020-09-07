04b326727b1cad39060468031c0803ab010bd90c
Sung <176462329@qq.com>
2020-09-07 20:17:35 +0800

    golang

---
+++ 01_Language/06_Go/src/tools/web01/main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", r.URL.Path)
+	})
+	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		bytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(r.RequestURI, err)
+			return
+		}
+		log.Println(r.RequestURI, string(bytes))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
